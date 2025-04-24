080c0d6090296f468c6c7c0d2155436b9eb19d57
Saddam Hussein <work.saddam786@gmail.com>
2025-04-24 23:34:38 +0530

    data types in js

---
+++ 01_Data_types.js
@@ -0,0 +1,31 @@
+/*
+There are two types of data types in java Script 
+    nn bb ss u
+1. Primitive = Null, Number, BigInt, Boolean, String, Symbol, Undefined.
+2. Non primitive =  Object [array, function...]
+*/
+
+let a = null;
+let b = 123;
+let c = true;
+let d = BigInt(576);
+let e = "Rock";
+let f = Symbol("I am Symbol");
+let g; // let g = undefined   //Both are same
+
+console.log(a, b, c, d, e, f, g);
+console.log(typeof c);
+
+// Objects
+const item = {
+  name: "Alex",
+  id: 23,
+  present: true,
+  result: undefined,
+};
+console.log(item);
+console.log(item.id);
+
+// Array
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
