0718953516a2d3097f8c2535ebaf29e8cda42ea2
Saddam Hussein <work.saddam786@gmail.com>
2025-05-01 22:31:42 +0530

    add more in string

--- 07_string.js
+++ 07_string.js
@@ -1,11 +1,13 @@
 //  Strings are IMMUTABLE *********
 
 let boy1 = "Alex"
-console.log(typeof boy1);
+console.log(typeof boy1);    //string
+console.log(boy1 instanceof String)   //false
 
 // using Constructor
 let boy2 = new String("Arin")
-console.log(typeof boy2);
+console.log(typeof boy2);     //object
+console.log(boy2 instanceof String)       //true
 
 // Template Literals (String Interpolation)
 let sen= `${boy1} is a friend of ${boy2}`       //using backtick
@@ -19,15 +21,17 @@ let sen= `${boy1} is a friend of ${boy2}`       //using backtick
 console.log(boy1.charAt(5))    //if out of index, nothing print.
 console.log(boy2.indexOf("x"))   // if given char/word not found,return: -1  
 
-console.log(sen.replace("friend","enemy"))  //replace only first occurence. use "replaceAll()" to replace all occurence.
+// console.log(sen.replace("friend","enemy"))  //replace only first occurence.
+//  use "replaceAll()" to replace all occurence.
 
 let t="   as of   now      "
-console.log(t.trim())   //remove extra white space from start & end only
+// console.log(t.trim())   //remove extra white space from start & end only
 
 let splitString = sen.split(" ")   //returrn the array or sub-string
-console.log(splitString)
+// console.log(splitString)
 
 // https://www.geeksforgeeks.org/difference-between-string-slice-and-string-substring-in-javascript/
 console.log(sen.substring(1,6))  //exclude last index
 console.log(sen.slice(1,6))   //exclude last index
 console.log(sen.length)
+
