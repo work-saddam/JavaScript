6c1b3a0a1e09380333de09c789c5a8c237279922
Saddam Hussein <work.saddam786@gmail.com>
2025-05-08 16:18:30 +0530

    reverse array & largest element in array

--- 08.2_array_problems.js
+++ 08.2_array_problems.js
@@ -1,4 +1,16 @@
-// Q1) Print every alternate element of the array starting from the first element.
+//** Q) Array Reverse
+function revArray(arr){
+    let n=arr.length
+    for(let i=0; i<=n/2; i++ ){
+        let temp = arr[i]
+        arr[i] = arr[n-1-i]
+        arr[n-1-i]=temp
+    }
+    return arr;
+}
+console.log(revArray([1, 4, 3, 2, 6, 5]))
+
+// Q) Print every alternate element of the array starting from the first element.
 function getAlternate(arr){
     let res=[];
     for(let i=0; i<arr.length; i+=2){
@@ -7,4 +19,26 @@ function getAlternate(arr){
     return res 
 }
 // console.log(getAlternate([1,2,3,9,6]));
+
+// Q) Print the Largest element in an Array
+function getLargest(arr){
+    let max = arr[0]
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i]
+        }
+    }
+    return max;
+}
+// console.log(getLargest([1,4,78,5,9]));
+
+//another way, not recommend for interview.
+function large(arr){
+    arr.sort( (a,b) => a-b )  //Numeric sort
+    // console.log(arr)   
+    return arr[arr.length-1]
+}
+// console.log(large([2,5,9,43,2,29]))
+
+
 
