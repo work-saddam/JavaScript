a631d64005e9908d1b7465a7b07596ff82c342e6
Saddam Hussein <work.saddam786@gmail.com>
2025-05-08 16:10:34 +0530

    recursive & itterative approach for factorail

--- 06.1_loops_problems.js
+++ 06.1_loops_problems.js
@@ -1,3 +1,23 @@
+// Q) Find the factorial.
+
+function factorial(n) {
+  // Recursive approach
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+
+  // Itterative approach
+  //   let res=1;
+  //   for(let i=2; i<=n; i++){
+  //     res=res*i
+  //   }
+  //   return res
+}
+console.log(factorial(5));
+
+
+
 // Q) Guess the number using a loop and switch statement.
 // Crearte html file and add the script to run it
 
@@ -35,123 +55,3 @@
 //     default:
 //       break;
 //   }
-
-// Pattern printing:-----------------------
-
-// Pattern-1
-// *
-// **
-// ***
-// ****
-// *****
-
-function p1(n) {
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= i; j++) {
-      process.stdout.write("*");
-    }
-    console.log();
-  }
-}
-// p1(5);
-
-// Pattern-2
-// *****
-// ****
-// ***
-// **
-// *
-
-function p2(n) {
-  for (let i = 1; i <= n; i++) {
-    for (let j = n; j >= i; j--) {
-      process.stdout.write("*");
-    }
-    console.log();
-  }
-}
-// p2(5);
-
-// Pattern-3
-//     *
-//    **
-//   ***
-//  ****
-// *****
-
-function p3(n) {
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= n - i; j++) {
-      process.stdout.write(" ");
-    }
-    for (let k = 1; k <= i; k++) {
-      process.stdout.write("*");
-    }
-    console.log();
-  }
-}
-// p3(5);
-
-// Pattern-4
-// 123
-// 456
-// 789
-
-let count = 1;
-function p4(n) {
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= n; j++) {
-      process.stdout.write(count.toString());
-      count++;
-    }
-    console.log();
-  }
-}
-// p4(3)
-
-// Pattern-5
-// *
-// **
-// ***
-// ****
-// *****
-// ****
-// ***
-// **
-// *
-
-function p5(n) {
-  for (let i = 1; i <= 2 * n - 1; i++) {
-    if (i <= n) {
-      for (let j = 1; j <= i; j++) {
-        process.stdout.write("*");
-      }
-    } else {
-      for (let k = 1; k <= 2 * n - i; k++) {
-        process.stdout.write("*");
-      }
-    }
-
-    console.log();
-  }
-}
-// p5(5);
-
-// Pattern-6
-//     *
-//    ***
-//   *****
-//  *******
-// *********
-function p6(n) {
-  for (let i = 1; i <= n; i++) {
-    for (let k = 1; k <= n - i; k++) {
-      process.stdout.write(" ");
-    }
-    for (let j = 1; j <= 2 * i - 1; j++) {
-      process.stdout.write("*");
-    }
-    console.log();
-  }
-}
-// p6(5);
